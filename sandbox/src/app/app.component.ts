bc16b9b55c53ed6d3441e7baa229746105bbdc9a
the-coding-panda <5512406+the-coding-panda@users.noreply.github.com>
2019-07-09 21:52:41 +0100

    Tuesday study time

--- sandbox/src/app/app.component.ts
+++ sandbox/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: 'pm-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+      </div>`
 })
+
 export class AppComponent {
-  title = 'sandbox';
+  pageTitle: string = 'Acme Product Management';
 }
